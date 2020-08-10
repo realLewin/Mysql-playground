4621eb43c245fee23dc7dbcb75e5d6f6a7ecb0f2
integreator <walterfrontier@gmail.com>
2020-08-10 07:09:20 -0400

    data insertion section complete

--- database-course/2.inserting-data/4.challenge.sql
+++ database-course/2.inserting-data/4.challenge.sql
@@ -7,3 +7,19 @@
 -- age - number mandatory
 -- current_status - text, mandatory, defaults to 'employed'
 
+CREATE DATABASE course;
+USE course;
+
+CREATE TABLE employees(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  last_name VARCHAR(50) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  middle_name VARCHAR(50),
+  age INT NOT NULL,
+  current_status VARCHAR(50) NOT NULL DEFAULT 'employed',
+  -- PRIMARY KEY (id) -- The equal to set primary key;
+);
+
+-- test:
+INSERT INTO employees(first_name, last_name, age) VALUES
+('Dora', 'Smith', 58);
